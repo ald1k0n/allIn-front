60b9af14e51b581c6bc32672917a637416602864
ALDIK <cjube1337@gmail.com>
2023-12-25 23:37:09 +0600

    service fix

--- src/redux/baseQuery.ts
+++ src/redux/baseQuery.ts
@@ -1,43 +1,43 @@
-import { fetchBaseQuery } from  '@reduxjs/toolkit/dist/query';
-import { baseURL } from "../configs";
-import { BaseQueryApi, BaseQueryArg, BaseQueryExtraOptions } from "@reduxjs/toolkit/dist/query/baseQueryTypes";
-import { logout, setAccessToken } from "./slices/user.slice.ts";
+import { fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { baseURL } from '../configs';
+
+import { logout, setAccessToken } from './slices/user.slice.ts';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: baseURL,
-  prepareHeaders: (header: { set: (arg0: string, arg1: string) => void; }, {getState}: any) => {
-    const access_token = getState().user.state.accessToken;
-    if (access_token) {
-      header.set('authorization', `Bearer ${access_token}`);
-    }
-    return header;
-  }
+	baseUrl: baseURL,
+	prepareHeaders: (header) => {
+		const access_token = localStorage.getItem('accessToken');
+		if (access_token) {
+			header.set('authorization', `Bearer ${access_token}`);
+		}
+		return header;
+	},
 });
 
 export const baseQueryReAuth = async (
-  args: BaseQueryArg<any>,
-  api: BaseQueryApi,
-  extraOptions: BaseQueryExtraOptions<any>
+	args: any,
+	api: any,
+	extraOptions: any
 ) => {
-  let result = await baseQuery(args, api, extraOptions as NonNullable<unknown>);
+	let result = await baseQuery(args, api, extraOptions as NonNullable<unknown>);
 
-  if (result?.error?.status === 401) {
-    console.log('sending refresh');
-    const refreshResult = await fetch(`${baseURL}/auth/refresh`, {
-        headers: {
-            'authorization': `Bearer ${api.getState().user.state.refreshToken}`
-        }
-    })
-        .then(res => res.json())
-        .catch(console.log);
+	if (result?.error?.status === 401) {
+		console.log('sending refresh');
+		const refreshResult = await fetch(`${baseURL}/auth/refresh`, {
+			headers: {
+				authorization: `Bearer ${api.getState().user.state.refreshToken}`,
+			},
+		})
+			.then((res) => res.json())
+			.catch(() => api.dispatch(logout()));
 
-    if (refreshResult.refreshToken) {
-      api.dispatch(setAccessToken(refreshResult.accessToken));
+		if (refreshResult) {
+			api.dispatch(setAccessToken(refreshResult.accessToken));
 
-      result = await baseQuery(args, api, extraOptions as NonNullable<unknown>);
-    } else {
-      api.dispatch(logout());
-    }
-  }
-  return result;
+			result = await baseQuery(args, api, extraOptions as NonNullable<unknown>);
+		} else {
+			api.dispatch(logout());
+		}
+	}
+	return result;
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,19 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
 import userSlice from './slices/user.slice';
 import interactionSlice from './slices/interaction.slice';
+import { chatTypeApi } from './services/chat.service';
 
 const rootReducers = combineReducers({
 	user: userSlice,
 	interaction: interactionSlice,
+	[chatTypeApi.reducerPath]: chatTypeApi.reducer,
 });
 
 export const store = configureStore({
 	reducer: rootReducers,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(chatTypeApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
