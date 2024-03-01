6c05a5d94ed8476b905779021c72595c4ad76836
Алдияр Сейлханов <seylkhanov.aldiyar@gmail.com>
2024-03-02 01:30:45 +0500

    check is admin, login via code

--- src/redux/services/users/users.service.ts
+++ src/redux/services/users/users.service.ts
@@ -1,57 +1,66 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import { baseQueryReAuth } from "@/redux/baseQuery";
-import { IUser } from "@/models";
+import { IUser } from '@/models';
+import { baseQueryReAuth } from '@/redux/baseQuery';
+import { createApi } from '@reduxjs/toolkit/query/react';
 
 export const userApi = createApi({
-  reducerPath: "userApi",
-  baseQuery: baseQueryReAuth,
-  tagTypes: ["Users"],
-  endpoints: (builder) => ({
-    getUsers: builder.query<{ data: IUser[]; count: number }, void>({
-      query: () => ({
-        url: "admin/users",
-      }),
-      providesTags: (result) =>
-        result
-          ? [
-              ...result.data.map(({ id }) => ({ type: "Users" as const, id })),
-              { type: "Users", id: "LIST" },
-            ]
-          : [{ type: "Users", id: "LIST" }],
-    }),
-    updateUsers: builder.mutation<undefined, any>({
-      query: (body) => {
-        console.log(body.get("id"));
-        return {
-          url: `admin/users/${body.get("id")}`,
-          method: "PATCH",
-          body,
-        };
-      },
-      invalidatesTags: ["Users"],
-    }),
-    deleteUser: builder.mutation<undefined, number>({
-      query: (id) => ({
-        url: `admin/users/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: ["Users"],
-    }),
+	reducerPath: 'userApi',
+	baseQuery: baseQueryReAuth,
+	tagTypes: ['Users'],
+	endpoints: (builder) => ({
+		getUsers: builder.query<{ data: IUser[]; count: number }, void>({
+			query: () => ({
+				url: 'admin/users',
+			}),
+			providesTags: (result) =>
+				result
+					? [
+							...result.data.map(({ id }) => ({ type: 'Users' as const, id })),
+							{ type: 'Users', id: 'LIST' },
+					  ]
+					: [{ type: 'Users', id: 'LIST' }],
+		}),
+		updateUsers: builder.mutation<undefined, any>({
+			query: (body) => {
+				console.log(body.get('id'));
+				return {
+					url: `admin/users/${body.get('id')}`,
+					method: 'PATCH',
+					body,
+				};
+			},
+			invalidatesTags: ['Users'],
+		}),
+		deleteUser: builder.mutation<undefined, number>({
+			query: (id) => ({
+				url: `admin/users/${id}`,
+				method: 'DELETE',
+			}),
+			invalidatesTags: ['Users'],
+		}),
 
-    createUser: builder.mutation<undefined, any>({
-      query: (body) => ({
-        url: "/admin/users",
-        method: "POST",
-        body,
-      }),
-      invalidatesTags: ["Users"],
-    }),
-  }),
+		createUser: builder.mutation<undefined, any>({
+			query: (body) => ({
+				url: '/admin/users',
+				method: 'POST',
+				body,
+			}),
+			invalidatesTags: ['Users'],
+		}),
+
+		sendCode: builder.mutation<undefined, { phone: string }>({
+			query: (body) => ({
+				url: '/auth/send-code',
+				method: 'POST',
+				body,
+			}),
+		}),
+	}),
 });
 
 export const {
-  useGetUsersQuery,
-  useUpdateUsersMutation,
-  useDeleteUserMutation,
-  useCreateUserMutation,
+	useGetUsersQuery,
+	useUpdateUsersMutation,
+	useDeleteUserMutation,
+	useCreateUserMutation,
+	useSendCodeMutation,
 } = userApi;
